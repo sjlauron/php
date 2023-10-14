387f51e5eb2df8ac0ddd9a079daa96550110b5cc
Sperry John Lauron <sperrylauron@gmail.com>
2023-10-14 10:26:38 +0800

    first commit

---
+++ MenuAct/ref_menu.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Oct 14, 2023 at 04:09 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos_lauron`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ref_menu`
+--
+
+CREATE TABLE `ref_menu` (
+  `id` int(8) NOT NULL,
+  `menu_name` varchar(255) NOT NULL,
+  `menu_desc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ref_menu`
+--
+
+INSERT INTO `ref_menu` (`id`, `menu_name`, `menu_desc`) VALUES
+(2, 'asd', 'asdasd'),
+(3, 'asd', 'asdasd'),
+(5, 'qwe', 'qwe');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ref_menu`
+--
+ALTER TABLE `ref_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ref_menu`
+--
+ALTER TABLE `ref_menu`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
